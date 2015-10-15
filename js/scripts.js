0338393830451013637d87b2d2cb3250eba31bc2
Audrey Scrugham <audrey@epicodus.com>
2015-10-15 13:33:02 -0700

    Change alert box to yes or no

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 $(function() {
-  var age = parseInt(prompt("How old are you?"));
-
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
+  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+  if (over21) {
     $('#drinks').show();
   } else {
     $('#under-21').show();
